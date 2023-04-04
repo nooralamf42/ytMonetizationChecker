ef5a1a3710fecf3b7cdc65e22a4d844dba5e6109
Noor Alam <nooralamf42@gmail.com>
2023-04-04 15:24:43 +0530

    YouTube Monetization Checker

--- myApp.js
+++ myApp.js
@@ -1,50 +1,59 @@
+
 // to call function when enter key is pressed on input field 
 
 channelLink.onkeydown = (event) => {
     if(event.key == "Enter")
       getData()
   }
-  // function to get data
-  
-  getData = () => {
-    if(channelLink.value.includes("https://www.youtube.com/")){
-      fetch('https://api.codetabs.com/v1/proxy?quest=' + channelLink.value)
-      .then((response) => {
-        return response.text();
-      }).then((data) => {
-        if (data.includes('"key":"is_monetization_enabled","value":"true"')){
-          messegeImg.src = "https://media.tenor.com/5FI2iWeIs70AAAAM/yes-yes-yes.gif"
-          output.innerHTML = "Channel is <span style='color: rgb(22, 188, 0);'>monetized</span>"
-        }
-        else{
-          messegeImg.src = "https://media.tenor.com/ciNDyf6AgH0AAAAd/disappointed-disappointed-fan.gif"
-          output.innerHTML = "Channel is <span style='color: rgb(255, 36, 36);'>not monetized</span>"
-  
-        }
-    })
+
+// to call function when get info button is clicked 
+
+getInfoBtn.onclick = () => {
+    getData()
+}
+
+// function to get data
+
+getData = () => {
+if(channelLink.value.includes("https://www.youtube.com/")){
+    fetch('https://api.codetabs.com/v1/proxy?quest=' + channelLink.value)
+    .then((response) => {
+    return response.text();
+    }).then((data) => {
+    if (data.includes('"key":"is_monetization_enabled","value":"true"')){
+        messegeImg.classList.remove("hideImg")
+        messegeImg.src = "https://media.tenor.com/5FI2iWeIs70AAAAM/yes-yes-yes.gif"
+        output.innerHTML = "Channel is <span style='color: rgb(22, 188, 0);'>monetized</span>"
     }
     else{
-      let orignalChannelLinkValue = channelLink.value
-      channelLink.value = "Invalid Channel Link!"
-      setTimeout(()=>{
-        channelLink.value = orignalChannelLinkValue
-      },1500)
+        messegeImg.classList.remove("hideImg")
+        messegeImg.src = "https://media.tenor.com/ciNDyf6AgH0AAAAd/disappointed-disappointed-fan.gif"
+        output.innerHTML = "Channel is <span style='color: rgb(255, 36, 36);'>not monetized</span>"
+
     }
-  }
-  
-  // to disable right click on body
-  
-  window.addEventListener("contextmenu", (e) => e.preventDefault())
-  
-  
-  // pasting link on right click on input field
-  
-  channelLink.addEventListener("contextmenu", () => {
-    navigator.clipboard
-    .readText()
-    .then(
-        cliptext =>
-            (channelLink.value = cliptext),
-    );
-  })
-  
+})
+}
+else{
+    let orignalChannelLinkValue = channelLink.value
+    channelLink.value = "Invalid Channel Link!"
+    setTimeout(()=>{
+    channelLink.value = orignalChannelLinkValue
+    },1500)
+}
+}
+
+// to disable right click on body
+
+window.addEventListener("contextmenu", (e) => e.preventDefault())
+
+
+// pasting link on right click on input field
+
+channelLink.addEventListener("contextmenu", () => {
+navigator.clipboard
+.readText()
+.then(
+    cliptext =>
+        (channelLink.value = cliptext),
+);
+})
