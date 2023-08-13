2debabbd7fe8236b6e785f989128fae06e5edb4a
nooralamf42 <nooralamf42@gmail.com>
2023-08-13 18:19:06 +0530

    added sound and fixed bugs

--- myApp.js
+++ myApp.js
@@ -1,3 +1,4 @@
+let audio = new Audio()
 
 // to call function when enter key is pressed on input field 
 
@@ -9,23 +10,33 @@ channelLink.onkeydown = (event) => {
 // to call function when get info button is clicked 
 
 getInfoBtn.onclick = () => {
-    getData()
+    setTimeout(
+        getData(),2000
+    )
 }
 
 // function to get data
 
 getData = () => {
 if(channelLink.value.includes("youtube.com/")){
+    messegeImg.classList.add("hideImg")
+    output.innerHTML = ""
+    loadingCircle.classList.remove("hideImg")
     fetch('https://api.codetabs.com/v1/proxy?quest=' + channelLink.value)
     .then((response) => {
     return response.text();
     }).then((data) => {
+        loadingCircle.classList.add("hideImg")
     if (data.includes('"key":"is_monetization_enabled","value":"true"')){
+        audio.src = "mon.mp3"
+        audio.play()
         messegeImg.classList.remove("hideImg")
-        messegeImg.src = "https://media.tenor.com/5FI2iWeIs70AAAAM/yes-yes-yes.gif"
+        messegeImg.src = "https://media.tenor.com/E6WSW38Kn94AAAAC/kbc-great.gif"
         output.innerHTML = "Channel is <span style='color: rgb(22, 188, 0);'>monetized</span>"
     }
     else{
+        audio.src = "notMon.mp3"
+        audio.play()
         messegeImg.classList.remove("hideImg")
         messegeImg.src = "https://media.tenor.com/ciNDyf6AgH0AAAAd/disappointed-disappointed-fan.gif"
         output.innerHTML = "Channel is <span style='color: rgb(255, 36, 36);'>not monetized</span>"
@@ -41,19 +52,3 @@ else{
     },1500)
 }
 }
-
-// to disable right click on body
-
-// window.addEventListener("contextmenu", (e) => e.preventDefault())
-
-
-// pasting link on right click on input field
-
-// channelLink.addEventListener("contextmenu", () => {
-// navigator.clipboard
-// .readText()
-// .then(
-//     cliptext =>
-//         (channelLink.value = cliptext),
-// );
-// })
